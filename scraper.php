4a43061432e9d83f96f0ebd862f5177cab579104
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-23 16:44:06 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,31 +1,4 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-
-
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
@@ -38,11 +11,11 @@ require 'scraperwiki/simple_html_dom.php';
 
 		if ($Html) {
 
-			foreach ($Html->find("//*[@id='MyTable']/tbody/tr") as $element) {
+			foreach ($Html->find("//*[@id='MyTable']/tbody/tr/td") as $element) {
 				$RowNumb	+=	1;
 				if ($RowNumb != 0) {
-					$Link	=	$element->find('a[id="navDet"]', 0)->href;
-					$newlink = 'https://www.insolvencydirect.bis.gov.uk/eiir/' . $Link;
+				$Link	=	$element->find('a[id="navDet"]', 0)->href;
+				$newlink = 'https://www.insolvencydirect.bis.gov.uk/eiir/' . $Link;
 				$DetailPg				=	file_get_html($newlink);
 				 echo $info['Surname']  						= $DetailPg->find("//*[@id='frmCaseDetail']/table[2]/tbody/tr[1]/td[2]",0)->plaintext;				
 				 $info['Forename']  					= $DetailPg->find("//*[@id='frmCaseDetail']/table[2]/tbody/tr[2]/td[2]",0)->plaintext;
