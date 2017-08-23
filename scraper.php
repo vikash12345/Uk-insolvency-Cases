23b140027106bfe026e7b1acb3c1f19afc3039bc
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-23 13:07:15 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -43,8 +43,66 @@ require 'scraperwiki/simple_html_dom.php';
 				if ($RowNumb != 0) {
 					$Link	=	$element->find('a[id="navDet"]', 0)->href;
 					$newlink = 'https://www.insolvencydirect.bis.gov.uk/eiir/' . $Link;
-					$DetailPg	=	file_get_html($newlink);
-					echo $surname  = $DetailPg->find("/*[@id='frmCaseDetail']/table[2]/tbody/tr[1]/td[2]",0)->plaintext;
+					$DetailPg				=	file_get_html($newlink);
+				$Surname  					= $DetailPg->find("/*[@id='frmCaseDetail']/table[2]/tbody/tr[1]td[2]",0)->plaintext;;
+				$Forename(s)  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[2]/tbody/tr[2]td[2]",0)->plaintext;;
+				$Title 						= $DetailPg->find("//*[@id="frmCaseDetail"]/table[2]/tbody/tr[3]td[2]",0)->plaintext;;
+				$Gender  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[2]/tbody/tr[4]td[2]",0)->plaintext;;
+				$Occupation 					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[2]/tbody/tr[5]td[2]",0)->plaintext;;
+				$DOB  						= $DetailPg->find("//*[@id="frmCaseDetail"]/table[2]/tbody/tr[6]td[2]",0)->plaintext;;
+				$Last_Known_Address  				= $DetailPg->find("//*[@id="frmCaseDetail"]/table[2]/tbody/tr[7]td[2]",0)->plaintext;
+				$Case_Name  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[1]td[2]",0)->plaintext;
+				$Court  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[2]td[2]",0)->plaintext;
+				$Type  						= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[4]td[2]",0)->plaintext;
+				$Number  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[5]td[2]",0)->plaintext;
+				$Case_Year  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[6]td[2]",0)->plaintext;
+				$Order_Date  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[7]td[2]",0)->plaintext;
+				$Status 					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[8]td[2]",0)->plaintext;
+				$Case_Description  				= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[9]td[2]",0)->plaintext;
+				$Main_Insolvency_Practitioner	  		= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[12]td[2]",0)->plaintext;
+				$Firm						= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[13]td[2]",0)->plaintext;
+				$Address  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[14]td[2]",0)->plaintext;
+				$Post_Code  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[15]td[2]",0)->plaintext;
+				$Telephone  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[16]td[2]",0)->plaintext;
+				$Insolvency_Service_Office  			= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[19]td[2]",0)->plaintext;
+				$Contact  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[20]td[2]",0)->plaintext;
+				$Address2  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[21]td[2]",0)->plaintext;
+				$Post_Code2 					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[22]td[2]",0)->plaintext;
+				$Telephone2  					= $DetailPg->find("//*[@id="frmCaseDetail"]/table[3]/tbody/tr[23]td[2]",0)->plaintext;
+					
+					
+					
+					scraperwiki::save_sqlite(array('mci_snum','page'), 
+    array('mci_snum' => $page, 
+          'Surname' => (trim($Surname)), 
+          'Forename(s)' => (trim($Forename(s))),
+          'Title' => (trim($Title)),
+          'Gender' => (trim($Gender)),
+          'Occupation' => (trim($Occupation)),
+          'DOB' => (trim($DOB)),
+          'Last_Known_Address' => (trim($Last_Known_Address)),
+          'Case_Name' => (trim($Case_Name)),
+          'Court' => (trim($Court)),
+			'Type' => (trim($Type)),
+			'Number' => (trim($Number)),
+          'Case_Year' => (trim($Case_Year)),
+			'Order_Date' => (trim($Order_Date)),
+			'Status' => (trim($Status)),
+          'Case_Description' => (trim($Case_Description)),
+			'Main_Insolvency_Practitioner' => (trim($Main_Insolvency_Practitioner)),
+			'Firm' => (trim($Firm)),
+          'Address' => (trim($Address)),
+			'Post_Code' => (trim($Post_Code)),
+			'Telephone' => (trim($Telephone)),
+			'Insolvency_Service_Office' => (trim($Insolvency_Service_Office)),
+			'Contact' => (trim($Contact)),
+			'Address2' => (trim($Address2)),
+			'Post_Code2' => (trim($Post_Code2)),
+			'Telephone2' => (trim($Telephone2))
+    ));
+    
+  //clean out the dom
+  $link2->__destruct();
 				}
 			}
 		}}
