13a055e0df8141ecb055b408936f01d5372047a4
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-23 11:13:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -38,7 +38,7 @@ require 'scraperwiki/simple_html_dom.php';
 
 		if ($Html) {
 
-			foreach ($Html->find("//*[@id='MyTable']/tbody") as $element) {
+			foreach ($Html->find("//*[@id='MyTable']/tbody/tr") as $element) {
 				$RowNumb	+=	1;
 				if ($RowNumb != 0) {
 					echo $Link	=	$element->find('a[id="navDet"]', 0)->href;
